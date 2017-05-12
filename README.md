050a594df04e3565c829885695d48ac3fc7107ed
Cloud User <centos@ip-172-31-40-230.us-west-2.compute.internal>
2017-05-12 16:50:13 +0000

    First release

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # docker-piler-php
 Piler web ui as Docker image (http://mailpiler.org)
+
+## Configure New Installation:
+```bash
+docker run -ti --rm -v /usr/local/etc/piler/:/usr/local/etc/piler:ro -v /var/www/piler/:/var/www/piler:rw heri16/piler-php
+# gather_webserver_data
+# gather_mysql_account
+# gather_smtp_relay_data
+```
+
+## Usage in docker-compose.yml:
+```yaml
+services:
+  php_fpm:
+    build: ./docker-piler-php
+    links:
+      - nginx_proxy
+      - piler_indexer
+    expose:
+      - "9000"
+    volumes:
+      - /var/www/piler/:/var/www/piler:ro
+      - /var/www/piler/tmp/:/var/www/piler/tmp:rw
+      - /usr/local/etc/piler/:/usr/local/etc/piler:ro
+      - /var/piler/store/:/var/piler/store:ro
+      - /var/piler/stat/:/var/piler/stat:ro
+    restart: always
+```
---
+++ run.sh
@@ -0,0 +1,49 @@
+#!/bin/sh -e
+[ -n "$DEBUG" ] && set -x
+
+pid=0
+
+# SIGTERM-handler
+term_handler() {
+  if [ $pid -ne 0 ]; then
+    kill -SIGTERM "$pid"
+    wait "$pid"
+  fi
+  exit 143; # 128 + 15 -- SIGTERM
+}
+
+# First-time config
+if [ ! -f /var/www/piler/config-site.php ]; then
+  apk add --no-cache mysql-client
+  sed -e'/load_default_values$/q' /usr/src/piler/util/postinstall.sh > /tmp/postinstall.sh
+  cd /usr/src/piler
+  (set +e
+    source /tmp/postinstall.sh
+    gather_webserver_data
+    gather_mysql_account
+    gather_smtp_relay_data
+    show_summary
+    if [ -d webui ]; then
+      echo -n "Copying www files to $DOCROOT... "
+      mkdir -p $DOCROOT || exit 1
+      cp -R webui/* $DOCROOT
+      cp -R webui/.htaccess $DOCROOT
+    fi
+    webui_install
+    clean_up_temp_stuff
+  )
+fi
+
+# setup handlers
+# on callback, kill the last background process, which is `php-fpm` and execute the specified handler
+trap 'kill ${!}; term_handler' SIGTERM
+
+# Start crond for piler tasks
+if [ -f /usr/local/etc/piler/crontab.piler ]; then
+  crontab -u piler /usr/local/etc/piler/crontab.piler
+fi
+crond
+pid="$!"
+
+# Start php-fpm
+php-fpm
